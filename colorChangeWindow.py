170df9f6f94a432d24de26b42c1e64b4339e6b4e
maxmyth01 <maxmyth01@gmail.com>
2017-10-16 14:54:15 -0400

    Updated from Brython Server: 10/16/2017 2:54:14 PM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -2,8 +2,13 @@
 #10-16-17
 #colorChangeWindow.py -- every click changes the color
 
+from ggame import *
 from random import randint
 
+red = Color(0xff0000,1)
+
+def mouseClick(event):
+    
 
 
 
