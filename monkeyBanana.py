2009fbbcef5f507fcb213f48e0054c4f40301f38
maxmyth01 <maxmyth01@gmail.com>
2017-10-19 14:10:27 -0400

    Updated from Brython Server: 10/19/2017 2:10:26 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -2,4 +2,14 @@
 #10-19-17
 #monkeyBanana.py - best game ever
 
+from ggame import *
 
+if __name__ == '__main__':
+    
+    green = Color(0x006600,1)
+    
+    jungleBox = RectangleAsset(800,600,LineStyle(1,green),green)
+    
+    Sprite(jungleBox)
+    
+    App().run()
