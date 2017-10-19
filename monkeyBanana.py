8a4c3c73057115d9b52511363b31e66673eea759
maxmyth01 <maxmyth01@gmail.com>
2017-10-19 14:26:35 -0400

    Updated from Brython Server: 10/19/2017 2:26:33 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -13,6 +13,11 @@ def moveRight(event):
     monkey.x += CELL_SIZE
 def moveLeft(event):
     monkey.x -= CELL_SIZE
+def moveUp(event):
+    monkey.y += CELL_SIZE
+def moveDown(event):
+    monkey.y -= CELL_SIZE
+
 
 if __name__ == '__main__':
     
@@ -30,4 +35,7 @@ if __name__ == '__main__':
     
     App.listenKeyEvent('keydown','right arrow', moveRight)
     App.listenKeyEvent('keydown','left arrow', moveLeft)
+    App.listenKeyEvent('keydown','up arrow', moveUp)
+    App.listenKeyEvent('keydown','down arrow', moveDown)
+
     App().run()
