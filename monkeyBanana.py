69a6b987e717483b014da607dd653e3cfc3735a6
maxmyth01 <maxmyth01@gmail.com>
2017-10-19 14:25:00 -0400

    Updated from Brython Server: 10/19/2017 2:24:59 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -11,6 +11,8 @@ CELL_SIZE = 20
 
 def moveRight(event):
     monkey.x += CELL_SIZE
+def moveLeft(event):
+    monkey.x -= CELL_SIZE
 
 if __name__ == '__main__':
     
@@ -27,4 +29,5 @@ if __name__ == '__main__':
     Sprite(bananaBox,(COLS*CELL_SIZE/2,ROWS*CELL_SIZE/2))
     
     App.listenKeyEvent('keydown','right arrow', moveRight)
+    App.listenKeyEvent('keydown','left arrow', moveLeft)
     App().run()
