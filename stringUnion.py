12238b5e0ee76dc9046db8456578368e304d3c84
maxmyth01 <maxmyth01@gmail.com>
2017-10-20 14:40:01 -0400

    Updated from Brython Server: 10/20/2017 2:39:59 PM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,13 @@
+#Max Low
+#10-20-17
+#stringUnion.py takes two words and then prints out all the apering letter each letter only once
+
+def stringUnion(word1, word2):
+    for ch in word1:
+        print ch 
+    for ch in word2: 
+        print ch
+    
+    
+stringUnion(mississippi,pennsylvania)
 
