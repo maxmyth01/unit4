5dc30a3a22d3951d2f1b691ffaf23784e6b9a657
maxmyth01 <maxmyth01@gmail.com>
2017-10-20 14:32:49 -0400

    Updated from Brython Server: 10/20/2017 2:32:47 PM

--- distance.py
+++ distance.py
@@ -1,1 +1,10 @@
+#Max Low
+#10-20-17
+#distance.py -- two poins and finds distance between
 
+from math import sqrt
+
+def distance(x1,y1,x2,y2):
+    print(((x2-x1)+(y2-y1))**0.5)
+    
+distance(3,4,-5,2)
