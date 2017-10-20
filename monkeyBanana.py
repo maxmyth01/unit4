b305b586977f2774bc5320a8492c61c8f1c68368
maxmyth01 <maxmyth01@gmail.com>
2017-10-20 14:27:41 -0400

    Updated from Brython Server: 10/20/2017 2:27:40 PM

--- monkeyBanana.py
+++ monkeyBanana.py
@@ -10,47 +10,54 @@ ROWS = 20
 COLS = 40
 CELL_SIZE = 20
 
+#moves the monkey right
 def moveRight(event):
     if monkey.x < (COLS-1)*CELL_SIZE:
         monkey.x += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
+#moves the monkey the left
 def moveLeft(event):
     if monkey.x > 0:
         monkey.x -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
+#moves the monkey the up
 def moveUp(event):
     if monkey.y > 0:
         monkey.y -= CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
+#moves the monkey the left
 def moveDown(event):
     if monkey.y < (ROWS-1)*CELL_SIZE:
         monkey.y += CELL_SIZE
         if monkey.x == banana.x and monkey.y == banana.y:
             moveBanana()
             updateScore()
-
+#moves the banana to random location
 def moveBanana():
     banana.x = randint(0,COLS-1)*CELL_SIZE
     banana.y = randint(0,ROWS-1)*CELL_SIZE
     data['frames'] = 0
-    
+#updates score when monkey gets banana
+#updates text on scene
 def updateScore():
     data['score'] += 10
     data['scoretext'].destroy()
     scoreBox = TextAsset('Score = '+str(data['score']))
     data['scoretext'] = Sprite(scoreBox,(0,ROWS*CELL_SIZE))
-    
+#keeps track of how many frames have past
+#moves banana if more than 150 frames have past
 def step():
     data['frames'] += 1
     if data['frames'] == 150:
         moveBanana()
 
+#runs the games
 if __name__ == '__main__':
     
     data = {}
