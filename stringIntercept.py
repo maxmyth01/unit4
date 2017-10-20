c1b446c7107e5c0d56080e713847af3d25ba45c0
maxmyth01 <maxmyth01@gmail.com>
2017-10-20 14:54:20 -0400

    Updated from Brython Server: 10/20/2017 2:54:19 PM

--- stringIntercept.py
+++ stringIntercept.py
@@ -1,1 +1,24 @@
+#Max Low
+#10-20-17
+#stringintercept.py takes two words and then prints out all the apering letter each letter only once
+
+def stringUnion(word1, word2):
+    letters1 = ''
+    letters2 = ''
+    letters3 = ''
+    for ch in word1:
+        if ch not in letters1:
+            letters1 = letters1 + ch
+    for ch in word2: 
+        if ch not in letters2:
+            letters2 = letters2 + ch
+            
+    for ch in letters1: 
+        if ch in letters2:
+            letters3 += ch
+    
+    return(letters3)
+    
+    
+print(stringUnion('mississippi','pennsylvania'))
 
