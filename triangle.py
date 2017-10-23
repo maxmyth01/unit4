221256638cc96428b151422897d60c26d0ef3465
maxmyth01 <maxmyth01@gmail.com>
2017-10-23 14:44:59 -0400

    Updated from Brython Server: 10/23/2017 2:44:59 PM

--- triangle.py
+++ triangle.py
@@ -1,1 +1,17 @@
+#Max Low
+#10-23-17
+#trangle.py -- uses 3 pt to determine area
 
+def distance(x1,y1,x2,y2):
+    print(((x2-x1)**2+(y2-y1)**2)**0.5)
+    
+
+def area(x1,y1,x2,y2,x3,y3):
+    a = distace(x1,y1,x2,y2)
+    b = distace(x2,y2,x3,y3)
+    c = distace(x3,y3,x1,y1)
+    s = 0.5(a+b+c)
+    return((s*(s-a)*(s-b)*(s-c))**0.5)
+
+
+print(area(3,4,-5,2,-7,1))
