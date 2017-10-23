e56ea19fc346f505103af78bb6ac8d397c49153f
maxmyth01 <maxmyth01@gmail.com>
2017-10-23 14:23:47 -0400

    Updated from Brython Server: 10/23/2017 2:23:47 PM

--- warmup11.py
+++ warmup11.py
@@ -1,1 +1,15 @@
+#Max Low
+#10-23-17
+#warmup11.py -- primes numbers
 
+def isPrime(num):
+    x=num - 1
+    while x > 1:
+        if num // x == 0:
+            return(False)
+        else:
+            x -= 1
+    return(True)
+    
+    
+isPrime(7)
