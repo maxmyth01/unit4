608cb1837be08aaf3934a88cb4678fe2de31f7cf
maxmyth01 <maxmyth01@gmail.com>
2017-10-23 14:19:15 -0400

    Updated from Brython Server: 10/23/2017 2:19:14 PM

--- localDemo.py
+++ localDemo.py
@@ -3,9 +3,12 @@
 #localDemo.py -- understanding local varibles
 
 def f():
-    x=77
+    x=77 # x is a local varible
+    y=10 # y is also a local varible
 
-#x=5
-f()
+
+x=5
+f() # x doesn't change
 print(x)
+print(y) # this will cause an error
 
