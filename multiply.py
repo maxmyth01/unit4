f3cea0bc8c3c273512e6fb35f6f7c3ca81350514
maxmyth01 <maxmyth01@gmail.com>
2017-10-23 14:55:10 -0400

    Updated from Brython Server: 10/23/2017 2:55:10 PM

--- multiply.py
+++ multiply.py
@@ -1,1 +1,20 @@
+#Max Low
+#10-23-17
+#multiply.py -- rand in 1-12 encourgaing message at 5 right
 
+
+def encouraging():
+    from random import rand int
+    x = randint(1,4)
+    if x= 1:
+        print(Great Job!)
+    elif x=2:
+        print(Keep it up!)
+    elif x=3:
+        print(Nice multiplying!)
+    elif x=4:
+        print(Nice work!)
+    
+
+
+while 
