ed1a2512f26b5d6b7272fbc37b0c2734f01739cb
maxmyth01 <maxmyth01@gmail.com>
2017-10-25 13:44:01 -0400

    Updated from Brython Server: 10/25/2017 1:44:00 PM

--- multiply.py
+++ multiply.py
@@ -23,7 +23,7 @@ while x<5:
     rand1 = randint(1,12)
     rand2 = randint(1,12)
     
-    answer = input('What is',rand1,'*',rand2,'?')
+    answer = input('What is'+rand1+'*'+rand2+'?')
     if answer == rand1*rand2:
         x += 5
     else:
