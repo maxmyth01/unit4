a150357deae9500a813f43820fdab6893c67a0e7
maxmyth01 <maxmyth01@gmail.com>
2017-10-27 14:30:46 -0400

    Updated from Brython Server: 10/27/2017 2:30:46 PM

--- warmup12.py
+++ warmup12.py
@@ -5,7 +5,7 @@
 def GCF(num1,num2):
     x = min(num1,num2)
     for i in range(x):
-        if min(num1,num2) % x == 0:
+        if min(num1,num2) % x == 0 and max(num1,num2) % x ==0:
             return x
         else:
             x -= 1
