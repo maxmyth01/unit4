3d3051a2f53870a2e08b9841d84abf44791242e1
maxmyth01 <maxmyth01@gmail.com>
2017-10-27 14:28:52 -0400

    Updated from Brython Server: 10/27/2017 2:28:52 PM

--- warmup12.py
+++ warmup12.py
@@ -1,1 +1,15 @@
+#Max Low
+#10-27-17
+#warmup12.py -- take two numbers and find the GCF
 
+def GCF(num1,num2):
+    x = min(num1,num2)
+    for i in range x:
+        if min(num1,num2) % x == 0:
+            return x
+        else:
+            x -= 1
+            
+print(GCF(10,15))
+        
+    
