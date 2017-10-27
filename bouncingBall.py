c8cd3af388ca8b0469e024919677038876ee3fdc
maxmyth01 <maxmyth01@gmail.com>
2017-10-27 14:43:21 -0400

    Updated from Brython Server: 10/27/2017 2:43:20 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -1,3 +1,15 @@
 #Max Low
 #10-26-17
 #bouncingBall.py -- 
+
+
+from random import randint
+from ggame import *
+
+red = Color(0xFF0000,1)
+blackOutline = LineStyle(5,black) # (pixels,colors)
+Circle = CircleAsset(radius,blackOutline,red) 
+
+Sprite(Circle,(x,y))
+
+App().run()
