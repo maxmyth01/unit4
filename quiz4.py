e137790fc7b654b1a40427e962287622a2df41b3
maxmyth01 <maxmyth01@gmail.com>
2017-10-30 14:33:09 -0400

    Updated from Brython Server: 10/30/2017 2:33:08 PM

--- quiz4.py
+++ quiz4.py
@@ -5,6 +5,10 @@
 def csia():
     for x in range(0,5):
         print("Computer Science is Awesome")
+
+def average(num1,num2,num3):
+    return (num1+num2+num3)/3
     
 
 csia()
+print(average(1,2,3))
