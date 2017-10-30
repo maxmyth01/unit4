e8dd5af866b82eb4d9b2ab115cc53a7f1648c2b7
maxmyth01 <maxmyth01@gmail.com>
2017-10-30 14:30:34 -0400

    Updated from Brython Server: 10/30/2017 2:30:33 PM

--- quiz4.py
+++ quiz4.py
@@ -1,1 +1,6 @@
+#Max Low
+#10-30-17
+#quiz4.py -- csia funtion, average function, string last letter function, true/false arguments same function
 
+def csia:
+    print("Computer Science is Awesome"*5)
