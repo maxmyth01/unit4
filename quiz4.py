5ed9945348beaa3a8203bcb5afc76ca0748bb706
maxmyth01 <maxmyth01@gmail.com>
2017-10-30 14:35:54 -0400

    Updated from Brython Server: 10/30/2017 2:35:53 PM

--- quiz4.py
+++ quiz4.py
@@ -8,7 +8,17 @@ def csia():
 
 def average(num1,num2,num3):
     return (num1+num2+num3)/3
-    
+
+def lastLetter(word):
+    x=0
+    y=0
+    for ch in word:
+        x += 1
+    for ch in word:
+        y +=1
+        if y==x:
+            print(ch)
 
 csia()
 print(average(1,2,3))
+lastLetter('Smeddinghoff')
